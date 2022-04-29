81b8075e0860aa6a8b8f82d4ecd351ba9f4a9585
mahedi <mahedianik4@gmail.com>
2022-04-30 03:48:44 +0600

    create put api update product by id

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ha8yc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -32,6 +33,24 @@ async function run() {
       const query = { _id: ObjectId(id) }
       const result = await productCollection.findOne(query);
       res.send(result)
+
+    });
+    // update item
+    app.put('/item/:id',async(req,res)=>{
+      const id = req.params.id;
+      const updateItems = req.body;
+      const quantity = updateItems.newQuantity
+      const filter = {  _id: ObjectId(id)};
+      const options = { upsert: true };
+
+      const updateDoc = {
+        $set: {
+          quantity :quantity
+        },
+      };
+      const result = await productCollection.updateOne(filter, updateDoc, options);
+      res.send(result)
+
 
     })
 
